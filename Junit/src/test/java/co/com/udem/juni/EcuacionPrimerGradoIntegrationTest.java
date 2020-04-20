51a764bba5666e3639b9b50a2839481757221cd7
Your Name <you@example.com>
2020-04-19 23:56:35 -0500

    se adicionan las clases de adicion con sus respectivos test

---
+++ Junit/src/test/java/co/com/udem/juni/EcuacionPrimerGradoIntegrationTest.java
@@ -0,0 +1,50 @@
+package co.com.udem.juni;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class EcuacionPrimerGradoIntegrationTest {
+	EcuacionPrimerGrado ecuacion = new EcuacionPrimerGrado();
+
+	@Test
+	public void solucionaEcuacionConMenos() {
+
+		Double result = ecuacion.obtenerResultado("2x - 1 = 0");
+
+		Double valueExpected = 0.5;
+
+		assertEquals(valueExpected, result);
+	}
+
+	@Test
+	public void solucionaEcuacionConMas() {
+
+		Double result = ecuacion.obtenerResultado("2x + 1 = 0");
+
+		Double valueExpected = -0.5;
+
+		assertEquals(valueExpected, result);
+	}
+
+	@Test
+	public void solucionaEcuacionConParte3Mayor0() {
+
+		Double result = ecuacion.obtenerResultado("2x + 1 = 10");
+
+		Double valueExpected = 4.5;
+
+		assertEquals(valueExpected, result);
+	}
+
+	@Test
+	public void solucionaEcuacionConParte3Menor0() {
+
+		Double result = ecuacion.obtenerResultado("2x + 1 = -10");
+
+		Double valueExpected = -5.5;
+
+		assertEquals(valueExpected, result);
+	}
+
+
+}
